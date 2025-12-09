e31c73787330f7dbf5abc4ffaadec606ffacbe4e
40404557 <40404557@HY-R104-22.resource.uc>
2025-12-09 12:48:29 +0000

    first commit

---
+++ src/main/java/com/mycompany/laxmia140404557/LaxmiA140404557.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laxmia140404557;
+
+/**
+ *
+ * @author 40404557
+ */
+public class LaxmiA140404557 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
